78f70572a9d56fa253e7cc2bf4ed2fc017cc73f4
Larissa Batista dos Santos <larissabatista0704@gmail.com>
2025-04-19 11:34:01 -0300

    Adição da classe histórico

---
+++ versao3.py/historico.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+class Historico():
+
+    def __init__(self):
+        self.__transacoes = list()
+    
+    @property
+    def transacoes(self):
+        return self.__transacoes
+
+    def adicionar_transacao(self, transacao):
+        self.__transacoes.append({
+            "tipo": transacao.__class__.__name__,
+            "valor": transacao.valor,
+            "data": datetime.now().strftime("%d/%m/%Y %H:%M:%s")
+        })
